dc7cb46d88c410880058b70aca4d0cb0bdf00ac0
MARuK-Ch <marikhasa1983@gmail.ru>
2025-12-27 21:42:17 +0500

    commit

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,7 @@ import 'dotenv/config'
 import { connectDB } from './core/db/db.js'
 import { createExpressApp } from './app.js'
 
-async function main() {
-  try {
-    await connectDB()
-    const app = createExpressApp()
-    const port = process.env.PORT || 5000
-    app.listen(port, () => console.log(`Server started! Listening on port ${port}`))
-  } catch (error) {
-    console.log(error)
-    console.log('Error when starting server')
-  }
+export async function createApp() {
+  await connectDB()
+  return createExpressApp()
 }
-
-void main()

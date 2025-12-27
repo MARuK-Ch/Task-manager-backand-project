dabdbaf9ccb9edfca371da952ac956ea96869448
MARuK-Ch <marikhasa1983@gmail.ru>
2025-12-27 21:15:41 +0500

    commit

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,25 @@
 import express from 'express'
 import cors from 'cors'
-import { taskRouter } from './features/task/task.routes.js'
+import {taskRouter} from './features/task/task.routes.js'
 import authRoutes from "./features/auth/auth.routes.js";
 
 export function createExpressApp() {
   const app = express()
 
-  const allowedOrigin = "https://task-manager-front-parcel-project.vercel.app"
-
-  // CORS — максимально корректная версия для Vercel
-  app.use(cors({
-    origin: allowedOrigin,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-  }))
-
-  // Обработка preflight (иначе POST ломается)
-  app.options("*", cors({
-    origin: allowedOrigin,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-  }))
+  const corsOptions = {
+      origin: process.env.CORS_ORIGIN,
+      optionsSuccessStatus: 200
+  }
 
   app.use(express.json())
+  app.use(cors(corsOptions))
+  app.use((_req, res, next) => {
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+    next()
+  })
 
   // тестовый запрос
-  app.get('/test', (_req, res) => {
-    res.json({ message: 'Server is working!' })
-  })
+  app.get('/test', (_req, res) => res.json({ message: 'Server is working!' }))
 
   app.use('/api/tasks', taskRouter)
   app.use('/api/auth', authRoutes)
